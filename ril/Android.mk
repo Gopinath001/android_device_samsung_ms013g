b3882dc7e69952663ff15e2c6660663ae87ecf37
gopinath001 <gopinath2nr@gmail.com>
2016-05-17 23:19:23 +0530

    already defined in common

--- ril/Android.mk
+++ ril/Android.mk
@@ -1,13 +1,13 @@
-LOCAL_PATH := $(call my-dir)
+#LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+#include $(CLEAR_VARS)
 
-LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
+#LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
 
-LOCAL_SRC_FILES := \
+#LOCAL_SRC_FILES := \
     ril_shim.c
 
-LOCAL_MODULE := libril_shim
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE := libril_shim
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
-include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_SHARED_LIBRARY)
